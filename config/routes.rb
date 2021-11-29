3d0376ab10745a372ed1cc13bd820d8059f37f61
pang yew ken <ukenster@gmail.com>
2021-11-29 18:07:50 +0800

    changed users shows form pages controller to useres controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get 'users/profile', to: 'pages#show', as: :user_profile
+  get 'users/profile', to: 'users#show', as: :user_profile
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

3637ddb689958ec146dbf01156601e9a9603111e
pang yew ken <ukenster@gmail.com>
2021-11-30 11:07:21 +0800

    routes for listings

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'users/profile', to: 'users#show', as: :user_profile
+  resources :listings, only: %i[show]
+
+  get 'listings/search', to: 'listings#search', as: :search_listing
+  get 'listings/filter', to: 'listings#filter', as: :filter_listing
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

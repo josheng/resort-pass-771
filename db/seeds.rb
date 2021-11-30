c76ed73e067cb4337ecb2d5deed050b9dcc6e6f7
Joshua Eng <engch28@gmail.com>
2021-11-30 10:20:43 +0800

    Created seed for listing DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning DB"
+Listing.destroy_all
+
+puts "Seeding the listing"
+
+20.times do
+  puts "Creating the listing
+  "
+  Listing.create!(
+    name: "#{Faker::Nation.capital_city} Hotel",
+    address: Faker::Address.street_address,
+    details: Faker::Lorem.paragraph(sentence_count: 10),
+    city_state: Faker::Nation.capital_city,
+    user_id: 1
+  )
+end
+
+puts "Seeding Done"

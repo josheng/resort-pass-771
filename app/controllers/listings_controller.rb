d69a3845bf26fbab0c9d8f5d2eca73eaf89a86a4
Joshua Eng <engch28@gmail.com>
2021-11-30 12:35:07 +0800

    Added search to home page and created search listing views with cards

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,13 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
+
+  def search
+    @listings = Listing.all
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      @listings = Listing.where("name ILIKE ?", "%#{@name}%")
+    end
+  end
 end

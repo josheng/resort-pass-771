6f03067a112c15b980d3f7d9307ca4068b230b64
Joshua Eng <engch28@gmail.com>
2021-12-01 11:50:29 +0800

    created review model and updated users and listings relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_074914) do
+ActiveRecord::Schema.define(version: 2021_12_01_034734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,17 @@ ActiveRecord::Schema.define(version: 2021_11_30_074914) do
     t.index ["listing_id"], name: "index_reservations_on_listing_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "listing_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["listing_id"], name: "index_reviews_on_listing_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -68,4 +79,6 @@ ActiveRecord::Schema.define(version: 2021_11_30_074914) do
   add_foreign_key "bookings", "users"
   add_foreign_key "listings", "users"
   add_foreign_key "reservations", "listings"
+  add_foreign_key "reviews", "listings"
+  add_foreign_key "reviews", "users"
 end

50ee83c7ccc380ffe57c6b80344e417ffc1a396e
pohpeiheng@gmail.com <pohpeiheng@gmail.com>
2021-12-01 15:53:34 +0800

    static map showing

--- app/javascript/packs/plugins/init_mapbox.js
+++ app/javascript/packs/plugins/init_mapbox.js
@@ -1,6 +1,13 @@
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  bounds.extend([markers.lng, markers.lat]);
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById("map");
 
@@ -11,6 +18,12 @@ const initMapbox = () => {
       container: "map",
       style: "mapbox://styles/mapbox/streets-v10",
     });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    new mapboxgl.Marker().setLngLat([markers.lng, markers.lat]).addTo(map);
+
+    fitMapToMarkers(map, markers);
+
   }
 
 };
